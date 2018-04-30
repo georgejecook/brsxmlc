580ec8c06937fcb5a9073eea098916afc9e73e3f
George Cook <george.cook@twintechs.com>
2018-04-29 21:34:35 -0500

    fixes processor, adds more tests

--- test/projectProcessorTest.js
+++ test/projectProcessorTest.js
@@ -12,6 +12,9 @@ const _ = require('lodash');
 const sourcePath = path.join(__dirname, 'stubProject');
 const targetPath = path.join(__dirname, '..', 'tmp');
 const importFilesPath = path.join(sourcePath, 'components', 'screens', 'imports');
+const chaiSubset = require('chai-subset');
+const chai = require('chai');
+chai.use(chaiSubset);
 
 describe("Project Processor", () => {
 	beforeEach(()=>{
@@ -65,11 +68,15 @@ describe("Project Processor", () => {
 	});
 
 	describe("Process files", () => {
+		beforeEach(()=>{
+			this.processor.copyFiles();
+			this.processor.createFileDescriptors(targetPath);
+			this.allFiles = this.processor.fileMap.allFiles;
+		});
+
+
 		it("populates descriptors",  () => {
 			//TODO test warnings and errors!
-
-			this.processor.copyFiles()
-			this.processor.createFileDescriptors(targetPath);
 			console.debug('finished processing map');
 			console.debug('warnings');
 			console.debug(this.processor.warnings);
@@ -81,47 +88,61 @@ describe("Project Processor", () => {
 
 		it("does not include excluded folders",  () => {
 			//TODO let config do this
-			this.processor.copyFiles();
-			this.processor.createFileDescriptors(targetPath);
-			const allFiles = this.processor.fileMap.allFiles;
-			console.debug('finished processing map - it contains');
-			expect(_.some(allFiles, { 'filename': 'test2importsExcluded.xml'})).to.be.true;
-			expect(_.some(allFiles, { 'filename': 'test2importsExcluded.xml'})).to.be.true;
 
+			expect(this.allFiles).not.contain.keys([
+				'test2.xml',
+				'test2importsExcluded.xml',
+				'test2importsExcluded.brs',
+				'testExcluded.brs'
+			]);
+
+			console.debug('finished processing map - it contains');
 			_.forOwn(this.processor.fileMap.allFiles ,(v, k) => console.debug(v.toString()));
 
 		});
 
 		it("does not include other filetypes",  () => {
-			this.processor.copyFiles();
-			this.processor.createFileDescriptors(targetPath);
-			console.debug('finished processing map - it contains');
-			_.forOwn(this.processor.fileMap.allFiles ,(v, k) => console.debug(v.toString()));
-
+			expect(_.some(this.allFiles, { 'fileTYPE': 'OTHTER'})).to.be.false;
 		});
 
 		it("correctly identifies brs files",  () => {
-			this.processor.copyFiles();
-			this.processor.createFileDescriptors(targetPath);
-			console.debug('finished processing map - it contains');
-			_.forOwn(this.processor.fileMap.allFiles ,(v, k) => console.debug(v.toString()));
+			expect(this.allFiles).containSubset({
+				'Utils.brs': (v) => v.filename === 'Utils.brs' && v.fileType === 'BRS',
+				'BadImport.brs': (v) => v.filename === 'BadImport.brs' && v.fileType === 'BRS',
+				'FocusMixin.brs': (v) => v.filename === 'FocusMixin.brs' && v.fileType === 'BRS',
+				'LogMixin.brs': (v) => v.filename === 'LogMixin.brs' && v.fileType === 'BRS',
+				'MultipleMixin.brs': (v) => v.filename === 'MultipleMixin.brs' && v.fileType === 'BRS',
+				'NetMixin.brs': (v) => v.filename === 'NetMixin.brs' && v.fileType === 'BRS',
+				'TextMixin.brs': (v) => v.filename === 'TextMixin.brs' && v.fileType === 'BRS'
+			});
 
 		});
 
 
 		it("correctly identifies xml files",  () => {
-			this.processor.copyFiles();
-			this.processor.createFileDescriptors(targetPath);
-			console.debug('finished processing map - it contains');
-			_.forOwn(this.processor.fileMap.allFiles ,(v, k) => console.debug(v.toString()));
-
+			const f = this.allFiles['testXMLOnly.xml'];
+			console.debug(f.fileType);
+			expect(this.allFiles).containSubset({
+				'testXMLOnly.xml': (v) => v.filename === 'testXMLOnly.xml' && v.fileType === 'XML'
+			});
 		});
 
-		it("correctly identifies codebehind files",  () => {
-			this.processor.copyFiles();
-			this.processor.createFileDescriptors(sourcePath);
-			console.debug('finished processing map - it contains');
-			_.forOwn(this.processor.fileMap.allFiles ,(v, k) => console.debug(v.toString()));
+		it("correctly identifies VIEWXML files",  () => {
+			expect(this.allFiles).containSubset({
+				'test.xml': (v) => v.filename === 'test.xml' && v.fileType === 'VIEWXML',
+				'test2imports.xml': (v) => v.filename === 'test2imports.xml' && v.fileType === 'VIEWXML',
+				'testcascadingimports.xml': (v) => v.filename === 'testcascadingimports.xml' && v.fileType === 'VIEWXML',
+				'testMissingImport.xml': (v) => v.filename === 'testMissingImport.xml' && v.fileType === 'VIEWXML',
+			});
+		});
+
+		it("correctly identifies CODEBEHIND files",  () => {
+			expect(this.allFiles).containSubset({
+				'test.brs': (v) => v.filename === 'test.brs' && v.fileType === 'CODEBEHIND',
+				'test2imports.brs': (v) => v.filename === 'test2imports.brs' && v.fileType === 'CODEBEHIND',
+				'testcascadingimports.brs': (v) => v.filename === 'testcascadingimports.brs' && v.fileType === 'CODEBEHIND',
+				'testMissingImport.brs': (v) => v.filename === 'testMissingImport.brs' && v.fileType === 'CODEBEHIND',
+			});
 		});
 	});
 });
